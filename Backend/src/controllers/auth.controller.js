1f65366372abf86521888fa39235087f343db1f7
Aaditya Jain <jainaaditya917@gmail.com>
2026-05-08 22:21:18 +0530

    Fixed auth cookies for production

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -35,7 +35,11 @@ async function registerUser(req, res) {
     { expiresIn: "7d" },
   );
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+   httpOnly: true,
+   secure: true,
+   sameSite: "none",
+});
 
   res.status(201).json({
     message: "User registered successfully",
