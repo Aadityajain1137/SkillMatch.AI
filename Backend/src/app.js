74fa2285b0b45635243bc373990585a233e84376
Aaditya Jain <jainaaditya917@gmail.com>
2026-05-08 22:13:32 +0530

    Updated production CORS

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-   origin: "https://skill-match-ai-six.vercel.app",
+   origin: [
+      "http://localhost:5173",
+      "https://skill-match-ai-six.vercel.app"
+   ],
    credentials: true
 }));
 
